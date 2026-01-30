c1b9fb0c5a284ca9b95b2a3a48ef09664cd2ebad
alakdjs <9765cass@naver.com>
2026-01-30 22:11:05 +0900

    일시정지 팝업에 버튼 추가
    타이틀 이동, 설정, 게임 종료 버튼 추가

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -8,4 +8,22 @@ public class SceneLoader : MonoBehaviour
     {
         SceneManager.LoadScene("SampleScene");
     }
+
+    public void GoStartScene()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+    // 게임 종료 버튼용
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        // 에디터에서 종료 테스트용
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // 빌드된 게임 종료
+        Application.Quit();
+#endif
+    }
+
 }
